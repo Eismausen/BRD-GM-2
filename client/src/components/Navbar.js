8f931da3dd779d8ec4b2e01c699e97e11aa5c3c6
David Avery <eismausen@gmail.com>
2022-01-17 16:19:11 -0800

    added static elements for Inventory, NavBar, Wishlist, and Browse endpoints; added dynamic routing (front)

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+function Navbar() {
+
+    return (
+        <div id="Navbar">
+            <small>Navbar layer :3</small>
+            <p>Sign-in</p>
+            <p>Inventory</p>
+            <p>Wishlist</p>
+            <p>Browse</p>
+            <p>Logout</p>
+        </div>
+    )
+}
+
+export default Navbar;
